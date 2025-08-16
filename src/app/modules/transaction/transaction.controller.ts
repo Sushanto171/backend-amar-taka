a3a9c99e037b1ce77d660a3a66508f032eeee241
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2025-08-16 21:21:57 +0600

    implement secure get transaction routes

---
+++ src/app/modules/transaction/transaction.controller.ts
@@ -0,0 +1,44 @@
+import { catchAsync } from "../../utils/catchAsync";
+import { httpsStatusCodes } from "../../utils/https-status-codes";
+import { sendResponse } from "../../utils/sendResponse";
+import { transactionService } from "./transaction.service";
+
+const getAllTransactions = catchAsync(async (req, res) => {
+  const transactions = await transactionService.getAllTransactions();
+  sendResponse(res, {
+    success: true,
+    statusCode: httpsStatusCodes.OK,
+    message: "All Transaction Retrieved Successfully.",
+    data: transactions,
+  });
+});
+const getTransactionByUserId = catchAsync(async (req, res) => {
+  const userId = req.user.userId;
+  const transactions = await transactionService.getTransactionByUserId(userId);
+  sendResponse(res, {
+    success: true,
+    statusCode: httpsStatusCodes.OK,
+    message: "All Transaction Retrieved Successfully.",
+    data: transactions,
+  });
+});
+const getSingleTransaction = catchAsync(async (req, res) => {
+  const transId = req.params.transactionId;
+  const userId = req.user.userId;
+  const transactions = await transactionService.getSingleTransaction(
+    userId,
+    transId
+  );
+  sendResponse(res, {
+    success: true,
+    statusCode: httpsStatusCodes.OK,
+    message: "Transaction Retrieved Successfully.",
+    data: transactions,
+  });
+});
+
+export const transactionController = {
+  getAllTransactions,
+  getTransactionByUserId,
+  getSingleTransaction,
+};
--- src/app/modules/transaction/transaction.routes.ts
+++ src/app/modules/transaction/transaction.routes.ts
@@ -1,5 +1,24 @@
 import { Router } from "express";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { IRole } from "../user/user.interface";
+import { transactionController } from "./transaction.controller";
 
 const router = Router();
+
+router.get(
+  "/",
+  checkAuth([...Object.values(IRole)]),
+  transactionController.getTransactionByUserId
+);
+router.get(
+  "/all-transactions",
+  checkAuth([IRole.ADMIN]),
+  transactionController.getAllTransactions
+);
+router.get(
+  "/:transactionId",
+  checkAuth([...Object.values(IRole)]),
+  transactionController.getSingleTransaction
+);
 
 export const TransactionRoutes = router;
