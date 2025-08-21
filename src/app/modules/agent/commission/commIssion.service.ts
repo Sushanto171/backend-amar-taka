a22a99433d5da98e464f64097dfd5ab7489ed663
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2025-08-21 22:52:50 +0600

    implement commission routes

--- src/app/modules/agent/commission/commIssion.service.ts
+++ src/app/modules/agent/commission/commIssion.service.ts
@@ -1,12 +1,29 @@
-import { Types } from "mongoose";
+import mongoose, { Types } from "mongoose";
 import { Commission } from "./commission.model";
 
+//admin route
+const getAllCommissions = async () => {
+  const commissions = await Commission.find({});
+  return commissions;
+};
 const getCommissions = async (userId: Types.ObjectId) => {
-  console.log(userId);
   const commissions = await Commission.find({ user: userId });
   return commissions;
 };
 
+const getSingleCommission = async (
+  userId: Types.ObjectId,
+  commissionId: string
+) => {
+  const commission = await Commission.findOne({
+    id: new mongoose.Types.ObjectId(commissionId),
+    user: userId,
+  });
+  return commission;
+};
+
 export const commissionService = {
+  getAllCommissions,
   getCommissions,
+  getSingleCommission,
 };
--- src/app/modules/agent/commission/commission.controller.ts
+++ src/app/modules/agent/commission/commission.controller.ts
@@ -1,7 +1,17 @@
 import { catchAsync } from "../../../utils/catchAsync";
 import { httpsStatusCodes } from "../../../utils/https-status-codes";
 import { sendResponse } from "../../../utils/sendResponse";
-import { commissionService } from "./commission.service";
+import { commissionService } from "./commIssion.service";
+
+const getAllCommissions = catchAsync(async (req, res) => {
+  const commissions = await commissionService.getAllCommissions();
+  sendResponse(res, {
+    success: true,
+    statusCode: httpsStatusCodes.OK,
+    message: "Retrieved all commissions successfully.",
+    data: commissions,
+  });
+});
 
 const getCommissions = catchAsync(async (req, res) => {
   const userId = req.user.userId;
@@ -14,6 +24,23 @@ const getCommissions = catchAsync(async (req, res) => {
   });
 });
 
+const getSingleCommission = catchAsync(async (req, res) => {
+  const userId = req.user.userId;
+  const commissionId = req.params.commissionId;
+  const commission = await commissionService.getSingleCommission(
+    userId,
+    commissionId
+  );
+  sendResponse(res, {
+    success: true,
+    statusCode: httpsStatusCodes.OK,
+    message: "Retrieved a commission successfully.",
+    data: commission,
+  });
+});
+
 export const commissionController = {
+  getAllCommissions,
   getCommissions,
+  getSingleCommission,
 };
