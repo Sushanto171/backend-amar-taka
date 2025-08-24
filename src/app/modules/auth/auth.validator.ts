e689b205f1442605c8dad517101edf28054047c8
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2025-08-24 21:26:01 +0600

    define zod schema change password route

--- src/app/modules/auth/auth.validator.ts
+++ src/app/modules/auth/auth.validator.ts
@@ -5,3 +5,12 @@ export const resetPasswordZodSchema = z.object({
   phone: z.string({ error: "Phone must be required" }),
   password: z.string({ error: "Password must be required" }),
 });
+
+export const verifyPwChangeOTPZodSchema = z.object({
+  otp: z.string({ error: "OTP must be required" }),
+});
+
+export const changePwZodSchema = z.object({
+  newPassword: z.string({ error: "New Password must be required" }),
+  oldPassword: z.string({ error: "Old Password must be required" }),
+});
